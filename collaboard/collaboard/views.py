1529a69eb2cf0894c8e4b8c4f1ab3aa370eb9c31
jovbk <jovani.badinga7@gmail.com>
2025-12-31 13:27:02 -0500

    Authentication flow is up and working fully.
    Reset password flow has been implemented. We can now confidently move onto the settings page functionality of the app

--- collaboard/collaboard/views.py
+++ collaboard/collaboard/views.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET"])
 def landing(request: HttpRequest) -> HttpResponse:
+    if request.user.is_authenticated:
+        return redirect("dashboard")
     return render(request=request, template_name="index.html", context={})
 
 
 @login_required
+@require_http_methods(["GET"])
 def dashboard(request: HttpRequest) -> HttpResponse:
     return render(request=request, template_name="dashboard.html", context={})
